9dd1eb4e236d4af71ec47eb83891f12d78e14a3d
Roberto Marino <rm.marinoroberto@gmail.com>
2025-04-01 15:23:52 +0200

    let's go react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Navbar, Container, Nav, NavDropdown} from "react-bootstrap"
 
 function App() {
   const [count, setCount] = useState(0)
