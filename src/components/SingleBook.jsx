95466457cf10760c472e9c78e909948677b97a5a
Roberto Marino <rm.marinoroberto@gmail.com>
2025-04-07 16:55:15 +0200

    Latest update

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -6,10 +6,15 @@ const SingleBook = ({ book, isSelected, onClick }) => (
     style={{
       border: isSelected ? '3px solid red' : '1px solid lightgray',
       cursor: 'pointer',
-      width: '200px'
+      width: '200px',
+      transition: 'border 0.2s ease-in-out'
     }}
   >
-    <Card.Img variant="top" src={book.img} style={{ height: '300px', objectFit: 'cover' }} />
+    <Card.Img
+      variant="top"
+      src={book.img}
+      style={{ height: '300px', objectFit: 'cover' }}
+    />
     <Card.Body>
       <Card.Title style={{ fontSize: '1rem' }}>{book.title}</Card.Title>
     </Card.Body>
